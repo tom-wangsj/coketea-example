0fd207e7d92d0d0ad7583e044ca14d62fe597d57
wangsj <wangshengjie2007@gmail.com>
2019-06-10 23:06:40 +0800

    添加cpu load冲高示例代码

---
+++ cpu-load/src/main/java/com/coketea/cpuload/App.java
@@ -0,0 +1,12 @@
+package com.coketea.cpuload;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        while (true) {
+            int a = 1 + 2;
+        }
+    }
+}
